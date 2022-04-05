0d4e518f9695197e45c89817feeeea70a7ac32a5
aldo-aguilar <aldoaguilar2022@u.northwestern.edu>
2022-04-04 23:39:06 -0600

    added new pattern for testing

--- fixture/fixture.ino
+++ fixture/fixture.ino
@@ -5,6 +5,16 @@
 #include "lfo.h"
 #include "snake.h"
 
+#ifdef BOARD_HAS_USB_SERIAL
+#include <SLIPEncodedUSBSerial.h>
+SLIPEncodedUSBSerial SLIPSerial( thisBoardsSerialUSB );
+#else
+#include <SLIPEncodedSerial.h>
+SLIPEncodedSerial SLIPSerial(Serial);
+#endif
+
+#include <vector> 
+
 // TODO (maybe): move this all into a setup file, and if you have time add a config 
 // TODO (maybe): file to fill out fields maybe?
 
@@ -13,10 +23,10 @@ std::vector<LightObject*> light_objs;
 
 // initalize panels and lighting objects for this fixture
 CRGB* object1_leds = new CRGB[NUM_LEDS_FIXTURE1];
-LightObject* light_obj1 = new Snake(0, 20, object1_leds, NUM_LEDS_FIXTURE1);
+LightObject* light_obj1 = new TwoSideFill(CRGB(255, 255, 0), object1_leds, NUM_LEDS_FIXTURE1);
 
 CRGB* object2_leds = new CRGB[NUM_LEDS_FIXTURE2];
-LightObject* light_obj2 = new Alternator(CRGB(255, 0, 0), object2_leds, NUM_LEDS_FIXTURE2);
+LightObject* light_obj2 = new TwoSideFill(CRGB(255, 0, 0), object2_leds, NUM_LEDS_FIXTURE2);
 
 // global serial port manager 
 // TODO: this is being declared here with the empty vec because I don't have a 
@@ -42,11 +52,41 @@ void setup() {
   delay(RECOVER_TIME);
 }
 
-void serialEvent() {
-//  spm.draw();
-  while (Serial.available()){
-    fixture.handle_serial_message();
+void OSCMsgReceive(){
+  OSCMessage msg;
+  
+   
+   while(!SLIPSerial.endofPacket()){
+    int size = SLIPSerial.available();
+    if (size > 0){
+      //fill the msg with all of the available bytes
+      while(size--){
+        msg.fill(SLIPSerial.read());
+      }
+    }
   }
+    
+  if(!msg.hasError()){
+    msg.route("/test", test);
+    msg.route("/vest", vest);
+  }
+
+}
+
+void test(OSCMessage& msg, int addressOffset) {
+  return;
+}
+
+void vest(OSCMessage& msg, int addressOffset) {
+  return;
+}
+
+void serialEvent() {
+  OSCMsgReceive();
+//  spm.draw();
+//  while (Serial.available()){
+//    fixture.handle_serial_message();
+//  }
 }
 
 void loop() { 
